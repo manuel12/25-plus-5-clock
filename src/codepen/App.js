f1a500e51c069ff54f52ce0659694c4af0e710a0
Manuel <manuelpinedacabeza@gmail.com>
2022-11-17 17:07:36 +0100

    Added codepen folder.

---
+++ src/codepen/App.js
@@ -0,0 +1,276 @@
+const TimeStepLengthController = (props) => {
+  return (
+    <div className="mb-3 col-md-6">
+      <div id={props.idLabel} className="heading-round bg-green">{props.textLabel}</div>
+      <div
+        id={props.decrementId}
+        className="button bg-orange"
+        onClick={props.decrementClickHandler}
+      >
+        Down
+      </div>
+      <div id={props.timeStepId} className="display-6">
+        {props.timeStepLength}
+      </div>
+      <div
+        id={props.incrementId}
+        className="button bg-orange"
+        onClick={props.incrementClickHandler}
+      >
+        Up
+      </div>
+    </div>
+  );
+};
+
+const TimerController = (props) => {
+  return (
+    <div className="timer-controls row">
+      <div
+        id="start_stop"
+        className="col-md-4 button bg-orange"
+        onClick={props.startStopClickHandler}
+      >
+        Play/Stop
+      </div>
+      <div className="col-md-1">
+      </div>
+      <div id="reset" className="col-md-4 button bg-orange" onClick={props.resetClickHandler}>
+        Reset
+      </div>
+    </div>
+  );
+};
+
+const DEFAULT_BREAK_LENGTH = 5;
+const DEFAULT_SESSION_LENGTH = 25;
+
+const convertTimerToMinutesAndSeconds = (timer) => {
+  let minutes = parseInt(timer / 60, 10);
+  let seconds = parseInt(timer % 60, 10);
+
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  const minutesAndSeconds = `${minutes}:${seconds}`;
+  return minutesAndSeconds;
+};
+
+const convertTimerToSeconds = (timer) => {
+  let timerString = String(timer);
+  if (!timerString.includes(":")) {
+    return parseInt(timerString * 60);
+  } else {
+    const [minutes, seconds] = timerString.split(":");
+    return parseInt(minutes * 60) + parseInt(seconds);
+  }
+};
+
+let intervalID = "";
+
+function App() {
+  const [breakLength, setBreakLength] = React.useState(DEFAULT_BREAK_LENGTH);
+  const [sessionLength, setSessionLength] = React.useState(DEFAULT_SESSION_LENGTH);
+
+  const [timerLabel, setTimerLabel] = React.useState(true);
+  const getTimerLabel = () => {
+    return timerLabel;
+  };
+  const [timer, setTimer] = React.useState(
+    convertTimerToMinutesAndSeconds(sessionLength * 60)
+  );
+
+  const [timerRunning, setTimerRunning] = React.useState(false);
+  const [timerStartingForFirstTime, setTimerStartingForFirstTime] =
+    React.useState(true);
+
+  const [timerInterval, setTimerInterval] = React.useState(null);
+  const [sessionNext, setSessionNext] = React.useState(false);
+
+  const [beep] = React.useState(document.getElementById("beep"));
+
+  const incrementDecrementHelper = (
+    lengthType,
+    increase = true,
+    setLengthFunc,
+    setTimer = null
+  ) => {
+    const currentLength = lengthType;
+    const updatedLength = increase ? currentLength + 1 : currentLength - 1;
+    if (updatedLength > 0 && updatedLength < 61) {
+      setLengthFunc(updatedLength);
+      setTimer && setTimer(convertTimerToMinutesAndSeconds(updatedLength * 60));
+    }
+  };
+
+  const breakDecrementClickHandler = () => {
+    incrementDecrementHelper(breakLength, false, setBreakLength);
+    console.log(timerLabel);
+  };
+
+  const breakIncrementClickHandler = () => {
+    incrementDecrementHelper(breakLength, true, setBreakLength);
+  };
+
+  const sessionDecrementClickHandler = () => {
+    incrementDecrementHelper(sessionLength, false, setSessionLength, setTimer);
+  };
+
+  const sessionIncrementClickHandler = () => {
+    incrementDecrementHelper(sessionLength, true, setSessionLength, setTimer);
+  };
+
+  const startActions = () => {
+    if (timerStartingForFirstTime) {
+      setTimerStartingForFirstTime(false);
+      startTimer(sessionLength * 60, timerLabel);
+    } else {
+      console.log(`Continuing with timer on: ${timer} ${typeof timer}`);
+      continueTimer(timer);
+    }
+  };
+
+  const stopActions = () => {
+    stopTimer();
+    console.log(`Stopped timer on: ${timer} ${typeof timer}`);
+  };
+
+  const startStopClickHandler = () => {
+    if (!timerRunning) {
+      setTimerRunning(true);
+      startActions();
+    } else {
+      setTimerRunning(false);
+      stopActions();
+    }
+  };
+
+  const resetClickHandler = () => {
+    setBreakLength(DEFAULT_BREAK_LENGTH);
+    setSessionLength(DEFAULT_SESSION_LENGTH);
+
+    setTimerLabel(true);
+    setTimer(convertTimerToMinutesAndSeconds(DEFAULT_SESSION_LENGTH * 60));
+
+    setTimerRunning(false);
+    setSessionNext(false);
+
+    clearInterval(intervalID);
+    stopBeep();
+  };
+
+  const timerFinished = (timerLabel) => {
+    playBeep();
+    clearInterval(intervalID);
+    setTimerInterval(null);
+
+    if (timerLabel) {
+      setTimer(convertTimerToMinutesAndSeconds(breakLength * 60));
+      startTimer(breakLength * 60, !timerLabel);
+      console.log(`Started new Break timer`);
+    } else {
+      setTimer(convertTimerToMinutesAndSeconds(sessionLength * 60));
+      startTimer(sessionLength * 60, !timerLabel);
+      console.log(`Started new Session timer`);
+    }
+  };
+
+  const createInterval = (timer, timerLabel) => {
+    return setInterval(function () {
+      if (--timer < 0) {
+        timer = 0;
+        timerFinished(timerLabel);
+        setTimerLabel(!timerLabel);
+      } else {
+        console.log(`timer: ${timer} - timerLabel: ${timerLabel}`);
+        setTimer(convertTimerToMinutesAndSeconds(timer));
+      }
+    }, 1000);
+  };
+
+  const startTimer = (duration, timerLabel) => {
+    let timer = duration;
+    intervalID = createInterval(timer, timerLabel);
+  };
+
+  const continueTimer = (duration) => {
+    let timer = convertTimerToSeconds(duration);
+    intervalID = createInterval(timer, timerLabel);
+  };
+
+  const stopTimer = () => {
+    clearInterval(intervalID);
+    setTimerInterval(null);
+  };
+
+  const playBeep = () => {
+    const audio = document.getElementById("beep");
+    audio.play();
+  };
+
+  const stopBeep = () => {
+    const audio = document.getElementById("beep");
+    audio.pause();
+    audio.currentTime = 0;
+  };
+
+  return (
+    <div className="App">
+      <div className="container app-container bg-blue">
+        <div className="title heading-round bg-red">
+          <h1>25 + 5 Clock</h1>
+        </div>
+        <div className="row">
+          <TimeStepLengthController
+            textLabel="Break Length"
+            idLabel="break-label"
+            decrementId="break-decrement"
+            decrementClickHandler={breakDecrementClickHandler}
+            incrementId="break-increment"
+            incrementClickHandler={breakIncrementClickHandler}
+            timeStepId="break-length"
+            timeStepLength={breakLength}
+          />
+
+          <TimeStepLengthController
+            textLabel="Session Length"
+            idLabel="session-label"
+            decrementId="session-decrement"
+            decrementClickHandler={sessionDecrementClickHandler}
+            incrementId="session-increment"
+            incrementClickHandler={sessionIncrementClickHandler}
+            timeStepId="session-length"
+            timeStepLength={sessionLength}
+          />
+        </div>
+
+        <div className="timer row">
+          <div id="timer-label" className="display-6 w-50 m-auto heading-round bg-green">
+            {timerLabel ? "Session" : "Break"}
+          </div>
+          <div id="time-left" className="display-1">
+            {timer}
+          </div>
+        </div>
+        <TimerController
+          startStopClickHandler={startStopClickHandler}
+          resetClickHandler={resetClickHandler}
+        />
+
+        <audio
+          id="beep"
+          preload="auto"
+          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+        ></audio>
+      </div>
+    </div>
+  );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
